6da62bc35e7289e7983937fb31c28e66589965c5
chenmin <wide_universe@foxmail.com>
2018-03-31 19:12:03 +0800

    sql窗口按钮没响应。

--- src/ui/ShowSqlDlg.java
+++ src/ui/ShowSqlDlg.java
@@ -1,11 +1,13 @@
 package ui;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.custom.StyledText;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Button;
 
 public class ShowSqlDlg extends Dialog {
 
@@ -15,45 +17,53 @@ public class ShowSqlDlg extends Dialog {
 
 	/**
 	 * Create the dialog.
+	 * 
 	 * @param parent
 	 * @param style
 	 */
 	public ShowSqlDlg(Shell parent, int style, String code) {
 		super(parent, style);
-		setText("SQL显示窗口");
+		this.setText("SQL显示窗口");
 		this.code = code;
 	}
 
 	/**
 	 * Open the dialog.
+	 * 
 	 * @return the result
 	 */
 	public Object open() {
-		createContents();
-		shlSql.open();
-		shlSql.layout();
-		Display display = getParent().getDisplay();
-		while (!shlSql.isDisposed()) {
+		this.createContents();
+		this.shlSql.open();
+		this.shlSql.layout();
+		Display display = this.getParent().getDisplay();
+		while (!this.shlSql.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
 		}
-		return result;
+		return this.result;
 	}
 
 	/**
 	 * Create contents of the dialog.
 	 */
 	private void createContents() {
-		shlSql = new Shell(getParent(), getStyle());
-		shlSql.setSize(675, 578);
-		shlSql.setText("SQL显示窗口");
-		
-		StyledText codeText = new StyledText(shlSql, SWT.BORDER);
+		this.shlSql = new Shell(this.getParent(), this.getStyle());
+		this.shlSql.setSize(675, 578);
+		this.shlSql.setText("SQL显示窗口");
+
+		StyledText codeText = new StyledText(this.shlSql, SWT.BORDER);
 		codeText.setBounds(10, 10, 649, 471);
-		codeText.setText(code);
-		
-		Button btnClose = new Button(shlSql, SWT.NONE);
+		codeText.setText(this.code);
+
+		Button btnClose = new Button(this.shlSql, SWT.NONE);
+		btnClose.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				ShowSqlDlg.this.shlSql.close();
+			}
+		});
 		btnClose.setBounds(272, 494, 114, 34);
 		btnClose.setText("关闭(&C)");
 
